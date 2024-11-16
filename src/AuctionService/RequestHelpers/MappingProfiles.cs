8d0347bfbc8ba15e8e4083f19e1b1d5ea0515a3f
m.shahid <muhammadshahid6528@gmail.com>
2024-11-16 05:25:06 +0500

    Add API Auction Crud

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Auction, AuctionDto>().IncludeMembers(x=> x.Item);
+            CreateMap<Item, AuctionDto>();
+            CreateMap<CreateAuctionDto, Auction>()
+                .ForMember(d=>d.Item,o => o.MapFrom(s => s));// here d for destination// s for source
+            CreateMap<CreateAuctionDto, Item>();
+        }
+    }
+}
